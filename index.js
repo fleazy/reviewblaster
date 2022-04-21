335c6bd555b1229f74e5e72bb389b62b20c4b53e
fleazy <dwilklives@gmail.com>
2022-04-21 17:59:07 -0500

    first real push of my code

---
+++ index.js
@@ -0,0 +1,142 @@
+const puppeteer = require('puppeteer')
+const chalk = require('chalk')
+
+async function scrapeIt() {
+
+    const browser = await puppeteer.launch({
+        args: ['--disable-dev-shm-usage'],
+        headless: true
+    });
+    const page = await browser.newPage();
+
+
+    await page.goto(process.argv[2]);
+    page.waitForTimeout(5000)
+    page.waitForSelector('.reviews-summary__num-reviews-right-rail')
+    let msg = await page.evaluate(function () {
+        let msg = document.querySelector('.reviews-summary__num-reviews-right-rail').textContent
+        return msg;
+    });
+    console.log(chalk.cyan(msg));
+
+    let reviewRegex = new RegExp("\\\d{2,4}");
+    let reviewNumber = msg.match(reviewRegex)[0];
+    
+    let reviewStartNumber = 5;
+    //const reviewCount = await page.$('.reviews-summary__num-reviews-right-rail').textContent
+    page.waitForTimeout(5000)
+    await page.hover('#reviews');
+    await page.hover('#amenities');
+    await page.hover('#reviews');
+
+    page.on('console', consoleObj => {
+        if (consoleObj.type() === 'log') {
+            if (consoleObj.text().indexOf("Matched Text") > -1) {
+                console.log(chalk.black.bgYellow(consoleObj.text()));
+            }
+            else if (consoleObj.text().indexOf('Review Page') > -1) {
+                console.log(chalk.black.bgWhite(consoleObj.text()));
+            }
+            else if (consoleObj.text().indexOf('w0rker') > -1) {
+                console.log(chalk.white(consoleObj.text()));
+            }
+            else{
+            console.log(chalk.cyan(consoleObj.text()));
+        }
+        }
+        
+    })
+
+
+
+
+    const links = await page.evaluate(() => {
+
+        setTimeout(() => {
+
+            // reviewCounter += document.querySelector('.reviews-summary__num-reviews-right-rail').textContent
+            // console.log(reviewCounter + " is the top review count")
+
+            everything();
+
+            function everything() {
+
+                let theseReviews = document.querySelectorAll('.review__content')
+                let uglyTitle = theseReviews[0].textContent.split('Stayed')[0]
+                let reviewContent = document.querySelectorAll('.review__content')[1].textContent.split('Stayed')[1].substring(9).slice(0,-4)
+
+                let regex = new RegExp(".{1}([\\\/]).");
+                let regexMatch = RegExp("\\\d{2,6}");
+                let descriptionRegex = RegExp("\\\d{3,6}");
+                let cleanTitle = uglyTitle.split(regex)[0];
+
+                theseReviews.forEach(function (el) {
+                    uglyTitle = el.textContent.split('Stayed')[0]
+                    cleanTitle = uglyTitle.split(regex)[0];
+                    let cleanerTitle = cleanTitle.replace(/2007|2008|2009|2010|2011|2012|2013|2014|2015|2016|2017|2018|2019|2020|2021|2022_/g,'');
+                    let reviewRange = document.querySelector('.pagination__text').textContent;
+                    console.log('checking ' + reviewRange)
+                    reviewContent = el.textContent.split('Stayed')[1].substring(9).slice(0,-13)
+                    if (cleanerTitle.match(regexMatch)) {
+                        console.log(cleanTitle + " FUCK YEAH")
+                        console.log("Review Page " + reviewRange)
+                    }
+                    if (reviewContent.match(descriptionRegex)) {
+                        let reviewMatch = reviewContent.match(descriptionRegex)[0]
+                        if (reviewMatch < 2006 || reviewMatch > 2022) {
+                            console.log(reviewMatch + " FUCK YEAH")
+                            console.log(`Matched Text: "${reviewContent}"`)
+                            console.log("Review Page " + reviewRange + " ^^^^^^^^^^^^^^^^^^^^^^^")
+                        }
+                        
+                    }
+                })
+            }
+
+            let nextButton = document.querySelector('#reviews > div > div > div > div > div > div.review-list > div.pagination > button.btn.btn-icon.ButtonIcon.btn-default.btn-sm.pagination__next.btn-icon-circle > span.SVGIcon.SVGIcon--16px.flex-center');
+            nextButton.onclick = function () {
+                setTimeout(() => {
+                    everything();
+                }, 1000);
+            }
+        }, 1000);
+    }); // 1. pass variable as an argument
+
+    page.waitForSelector('#reviews')
+    await page.click('#reviews')
+    page.waitForSelector('.reviews-summary__num-reviews-right-rail')
+
+    const hrefElement = await page.$$('.pagination__next');
+    //page.waitForSelector('.reviews-summary__reviews_count_small')
+    //const reviewCount = await page.$('.reviews-summary__reviews_count_small');
+    //await page.waitForTimeout(3000)
+    //let value = await reviewCount.evaluate(el => el.textContent)
+    await page.waitForTimeout(3000)
+    //console.log(value)
+    await hrefElement[0].click();
+    if (reviewNumber > 5) {
+
+        while (reviewStartNumber <= reviewNumber) {
+            await page.waitForTimeout(1000)
+            reviewStartNumber += 5;
+            if (reviewStartNumber >= reviewNumber){
+                break;
+            } else{
+                hrefElement[0].click();
+            }
+            
+
+        }
+        console.log("we're done");
+        page.close();
+        browser.close();
+    }
+         
+
+
+}
+
+
+
+scrapeIt();
+
---
+++ vrapihelper.ahk
@@ -0,0 +1,3 @@
+pageN := clipboard ;
+
+Run, %ComSpec% /k cd C:/Users/dwilk/Desktop/VRAPIhelper && node index.js %clipboard%
